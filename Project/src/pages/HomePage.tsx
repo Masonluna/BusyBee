67e84fd99f76c191d07eafd7be7c4e79c0f34c28
Jacqueline Justice <kobrakid3716@gmail.com>
2025-03-18 12:18:36 -0500

    edited footer

--- Project/src/pages/HomePage.tsx
+++ Project/src/pages/HomePage.tsx
@@ -36,7 +36,7 @@ const HomePage: React.FC = () => {
         <div>
             <WelcomeMessage authUser={authUser} /> 
             <EasyNav />
-            <Footer></Footer>
+            <Footer />
         </div>
     )
 }
