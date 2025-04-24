94424cae24ef262aaf366e4168819b12ee056733
Mason <47919925+OAOEnder@users.noreply.github.com>
2025-04-24 13:47:38 -0500

    fixing empty job list handling

--- Project/src/components/summaryDash/SummaryDashboard.tsx
+++ Project/src/components/summaryDash/SummaryDashboard.tsx
@@ -5,12 +5,12 @@ import StatsQuickView from "./StatsQuickView";
 const SummaryDashboard: React.FC = () => {
     // Use the context to get jobs and stats
     const { jobs, stats } = useDashboard();
-
+    console.log(`jobs=${jobs}, stats=${stats}`);
     return (
         <>
             <div>
-                { jobs && jobs.length > 0 && <JobsQuickView /> }
-                { stats && <StatsQuickView /> }
+                <JobsQuickView />
+                <StatsQuickView />
             </div>
         </>
     )
